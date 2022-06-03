c34f39da5cd1dc9642c15e7ea3c53258686d294c
RiceQuakes <2486921393@qq.com>
2022-06-03 23:24:53 +0800

    修复一些bug 优化弹道稀疏

--- src/main/java/lqw/coop/Features/Gun.java
+++ src/main/java/lqw/coop/Features/Gun.java
@@ -3,8 +3,11 @@ package lqw.coop.Features;
 import lqw.coop.Coop;
 import org.bukkit.*;
 import org.bukkit.entity.*;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -19,41 +22,49 @@ public class Gun implements Listener {
     }
 
     @EventHandler
-    public void onPlayerShoot(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.WOODEN_HOE) {
-            Vector vector = player.getLocation().getDirection();
-            vector.multiply(-1);
-            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 1, 1);
-
-            new BukkitRunnable() {
-                final Location parLoc = player.getEyeLocation();
-                final int speed = 5;
-
-                @Override
-                public void run() {
-                    for (int i = 1; i <= speed; i++) {
-                        player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, parLoc,
-                                1, 0, 0, 0, 0);
-                        if (checkAndDamageNearByEntities(player, parLoc)) {
-                            cancel();
-                            player.playSound(player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1);
-                            break;
-                        }
-                        parLoc.subtract(vector);
-                    }
-                    if (parLoc.distance(player.getLocation()) > 500 || parLoc.getBlock().getType() != Material.AIR)
-                        cancel();
-                }
-            }.runTaskTimer(plugin, 0, 1);
+    public void onPlayerInt(PlayerInteractEvent event) {
+        if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.WOODEN_HOE) {
+            shoot(event);
+            event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onPlayerRightClick(PlayerInteractEvent event) {
+    public void onPlayerIntEnt(PlayerInteractAtEntityEvent event) {
         if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.WOODEN_HOE) {
+            shoot(event);
             event.setCancelled(true);
         }
+    }
+
+    private void shoot(PlayerEvent event) {
+        Player player = event.getPlayer();
+        Vector vector = player.getLocation().getDirection();
+        vector.multiply(-1);
+        player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 1, 1);
+
+        new BukkitRunnable() {
+            final Location parLoc = player.getEyeLocation();
+            final int speed = 5;
+            int times = 0;
+
+            @Override
+            public void run() {
+                for (int i = 1; i <= speed; i++) {
+                    if (++times % 3 == 0) // 弹道稀疏
+                        player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, parLoc,
+                                1, 0, 0, 0, 0);
+                    if (checkAndDamageNearByEntities(player, parLoc)) {
+                        cancel();
+                        player.playSound(player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1);
+                        break;
+                    }
+                    parLoc.subtract(vector);
+                }
+                if (parLoc.distance(player.getLocation()) > 500 || parLoc.getBlock().getType() != Material.AIR)
+                    cancel();
+            }
+        }.runTaskTimer(plugin, 0, 1);
     }
 
     private boolean checkAndDamageNearByEntities(Player shooter, Location loc) {
