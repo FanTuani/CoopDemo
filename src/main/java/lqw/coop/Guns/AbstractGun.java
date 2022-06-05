678ad6e83e2255b8b5d91cef4d91317a12683c80
RiceQuakes <2486921393@qq.com>
2022-06-05 17:42:35 +0800

    修复了成吨的bug
    大概也创造了成吨的bug

--- src/main/java/lqw/coop/Guns/AbstractGun.java
+++ src/main/java/lqw/coop/Guns/AbstractGun.java
@@ -4,7 +4,6 @@ import lqw.coop.Coop;
 import lqw.coop.Game.Game;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -16,16 +15,15 @@ import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 import java.util.Collection;
 
 public abstract class AbstractGun implements Listener {
     protected final Coop plugin = Coop.instance;
     protected Material gunItemType;
-    protected int bulletSpeed, maxRange, coolDownTicks, damage, capacity;
+    protected int bulletSpeed, maxRange, coolDownTicks, reloadTicks, damage, capacity;
+    protected double hitBox;
 
     protected AbstractGun() {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
@@ -33,31 +31,58 @@ public abstract class AbstractGun implements Listener {
 
     abstract void shoot(PlayerEvent event);
 
-    private void reload(PlayerEvent event) {
+    private void beforeShoot(PlayerEvent event) {
         Player player = event.getPlayer();
-        if (!Game.getIsReloading(player)) {
-            Game.setIsReloading(player, true);
-            int maxDur = player.getInventory().getItemInMainHand().getType().getMaxDurability();
-            ItemStack item = player.getInventory().getItemInMainHand();
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        if (!Game.getIsReloading(player) && Game.getIsCooledOver(player) && Game.reduceCapacity(item, capacity)) {
+            // 若 不在换弹 且 射速允许 且 (弹药量-1 若弹药量!=0) 则开火
+            Game.setIsCooledOver(player, false);
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    int nowDur = Game.getDurability(item);
-                    if (nowDur >= maxDur) {
-                        Game.setIsReloading(player, false);
-                        cancel();
-                    }
-                    if (player.getInventory().getItemInMainHand().getType() == gunItemType) {
-                        Game.setDurability(item, nowDur + maxDur / capacity);
-                        player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 0.1F, 2F);
-                    }
+                    Game.setIsCooledOver(player, true);
                 }
-            }.runTaskTimer(plugin, 0, 2);
+            }.runTaskLater(plugin, coolDownTicks);
+            shoot(event);
+        } else if (!Game.getIsReloading(player) && Game.getIsCooledOver(player)) {
+            reload(event);
         }
     }
 
+    private void afterShoot(PlayerEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand();
+        int maxDur = item.getType().getMaxDurability();
+        if (Game.getDurability(item) < maxDur / capacity) reload(event); // 空弹夹自动换弹
+    }
+
+    private void reload(PlayerEvent event) {
+        Player player = event.getPlayer();
+        int maxDur = player.getInventory().getItemInMainHand().getType().getMaxDurability();
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (maxDur == Game.getDurability(item)) return;
+        Game.setIsReloading(player, true);
+        new BukkitRunnable() {
+            int nowDur = 0;
+
+            @Override
+            public void run() {
+                if (nowDur >= maxDur) {
+                    Game.setIsReloading(player, false);
+                    cancel();
+                }
+                if (player.getInventory().getItemInMainHand().getType() == gunItemType) {
+                    Game.setDurability(item, nowDur + maxDur / reloadTicks);
+                    nowDur = Game.getDurability(item);
+                    player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 0.1F, 2F);
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1);
+    }
+
     protected boolean checkAndDamageNearByEntities(Player shooter, Location loc, int damage) {
-        Collection<Entity> collection = loc.getWorld().getNearbyEntities(loc, 0.5, 0.5, 0.5);
+        Collection<Entity> collection = loc.getWorld().getNearbyEntities(loc, hitBox, hitBox, hitBox);
         if (collection.size() == 0) return false;
         else {
             for (Entity entity : collection) {
@@ -76,20 +101,25 @@ public abstract class AbstractGun implements Listener {
 
     @EventHandler
     public void onPlayerInt(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (event.getPlayer().getInventory().getItemInMainHand().getType() == gunItemType) {
-                shoot(event);
+        if (event.getPlayer().getInventory().getItemInMainHand().getType() == gunItemType) {
+            if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) { // 右键尝试射击
+                // 射击前后
+                beforeShoot(event);
+                afterShoot(event);
                 event.setCancelled(true);
+
+            } else if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) { // 左键换弹
+                if (!Game.getIsReloading(event.getPlayer())) // 防止打断换弹
+                    reload(event);
             }
-        } else {
-            reload(event);
         }
     }
 
     @EventHandler
     public void onPlayerIntEnt(PlayerInteractAtEntityEvent event) {
         if (event.getPlayer().getInventory().getItemInMainHand().getType() == gunItemType) {
-            shoot(event);
+            beforeShoot(event);
+            afterShoot(event);
             event.setCancelled(true);
         }
     }
